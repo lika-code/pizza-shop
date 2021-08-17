7c4162672e456dffe072693183474d0cfc25d93f
Lika <lika@gmail.com>
2021-08-17 15:18:59 +0300

    Added library Redux

---
+++ src/redux/store.js
@@ -0,0 +1,16 @@
+import { createStore } from "redux";
+
+function counterReducer(state = { value: 0 }, action) {
+    switch (action.type) {
+        case "ДОБАВИТЬ":
+            return { value: state.value + 1 };
+        case "ОТНЯТЬ":
+            return { value: state.value - 1 };
+        default:
+            return state;
+    }
+}
+
+const store = createStore(counterReducer);
+
+export default store;
